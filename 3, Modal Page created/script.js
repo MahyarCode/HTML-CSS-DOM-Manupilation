242eb3e5e2a408e8b5b17b52ab40571e32c3af51
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-10-26 13:18:53 +0330

    Refactored the Modal (script.js)

--- 3, Modal Page created/script.js
+++ 3, Modal Page created/script.js
@@ -8,20 +8,31 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 //DESC the above line, will select the all elements which is '.show-modal' class. the querySelector only select the first one.
 // console.log(btnsOpenModal);
 
+const closeModal = function () {
+  //DESC this function will ADD the 'hidden' class to the classList of the specified elements
+  /*
+      in HTML file:
+
+      class = 'modal' ==> class = 'modal hidden'
+  */
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  //DESC this function will REMOVE the 'hidden' class to the classList of the specified elements
+  /*
+      in HTML file:
+
+      class = 'modal hidden' ==> class = 'modal'
+  */
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
