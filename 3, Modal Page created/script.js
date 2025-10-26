c5bbce2ed2d600a6cca516b1274aaa79c47f68e9
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-10-26 13:12:14 +0330

    Added the Modal Page (HardCoded)

---
+++ 3, Modal Page created/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+//DESC the above line, will select the all elements which is '.show-modal' class. the querySelector only select the first one.
+// console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
